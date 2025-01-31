f88a49af50bf587e89a917980cc0acf53fe7748b
MSyaekhoni <syaekhonimuhammad@gmail.com>
2025-01-31 15:46:03 +0700

    konfigurasi tailwindcsss dan alpinejs

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+
+    <h1>Halaman Blog</h1>
+
+    <h3>Tulungagung Kota Cethe</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati adipisci omnis perferendis quaerat incidunt
+        dignissimos ipsa eveniet rerum minus, a, quisquam corporis iure hic beatae quibusdam animi voluptatem, nulla
+        natus.</p>
+</body>
+
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+
+    <h1>Halaman Contact</h1>
+    <ul>
+        <li>
+            <a href="https://www.instagram.com/m__syaekhoni/">Instagram</a>
+        </li>
+        <li>
+            <a href="https://www.linkedin.com/in/muhammad-syaekhoni-4427581a9/">LinkedIn</a>
+        </li>
+    </ul>
+</body>
+
+</html>
